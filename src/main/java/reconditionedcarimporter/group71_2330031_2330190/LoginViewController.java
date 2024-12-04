5d2e1b1cfacb7acf9737ad0b546f17304f14f172
zawad12 <2330031@iub.edu.bd>
2024-12-04 23:03:30 +0600

    controller fixed

--- src/main/java/reconditionedcarimporter/group71_2330031_2330190/LoginViewController.java
+++ src/main/java/reconditionedcarimporter/group71_2330031_2330190/LoginViewController.java
@@ -1,7 +1,20 @@
 package reconditionedcarimporter.group71_2330031_2330190;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class LoginViewController
 {
     @javafx.fxml.FXML
+    private TextField passwordTextField;
+    @javafx.fxml.FXML
+    private TextField usernameTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void loginButtonOnAction(ActionEvent actionEvent) {
+    }
+}
