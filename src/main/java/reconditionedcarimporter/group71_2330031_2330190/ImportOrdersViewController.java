dcba6bb064bdea51a4998f022c37ec5da34b299f
zawad12 <2330031@iub.edu.bd>
2024-12-06 20:32:00 +0600

    new fxml file added

---
+++ src/main/java/reconditionedcarimporter/group71_2330031_2330190/ImportOrdersViewController.java
@@ -0,0 +1,25 @@
+package reconditionedcarimporter.group71_2330031_2330190;
+
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class ImportOrdersViewController
+{
+    @javafx.fxml.FXML
+    private TextField carBrandTextField;
+    @javafx.fxml.FXML
+    private DatePicker expectedShipmentDatePicker;
+    @javafx.fxml.FXML
+    private TextField supplierNameTextField;
+    @javafx.fxml.FXML
+    private TextField carModelTextField;
+    @javafx.fxml.FXML
+    private TextField supplierIDTextfield;
+    @javafx.fxml.FXML
+    private TextField quantityTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+
+    }
+}
