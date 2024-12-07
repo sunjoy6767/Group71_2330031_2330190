bd4b229423c2c8978948f9efa015d66724043628
zawad12 <2330031@iub.edu.bd>
2024-12-07 20:29:19 +0600

    Update loginViewController

--- src/main/java/reconditionedcarimporter/group71_2330031_2330190/LoginViewController.java
+++ src/main/java/reconditionedcarimporter/group71_2330031_2330190/LoginViewController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -15,14 +16,28 @@ public class LoginViewController
     private TextField passwordTextField;
     @javafx.fxml.FXML
     private TextField usernameTextField;
+    @javafx.fxml.FXML
+    private ComboBox<String> loginAsComboBox;
 
     @javafx.fxml.FXML
     public void initialize() {
-
+        loginAsComboBox.getItems().addAll("CarImportManager", "MarketingManager",
+        "SalesRepresentative", "InventoryManager");
     }
 
     @javafx.fxml.FXML
     public void loginButtonOnAction(ActionEvent actionEvent) throws IOException {
-        SceneSwitcher.switchScene("MarketingManager-view.fxml", actionEvent);
+        if (loginAsComboBox.getValue().equals("CarImportManager")){
+            SceneSwitcher.switchScene("CarImportManager-view.fxml", actionEvent);
+        }
+        else if (loginAsComboBox.getValue().equals("MarketingManager")){
+            SceneSwitcher.switchScene("MarketingManager-view.fxml", actionEvent);
+        }
+        else if (loginAsComboBox.getValue().equals("SalesRepresentative")){
+            SceneSwitcher.switchScene("SalesRepresentative-view.fxml", actionEvent);
+        }
+        else if (loginAsComboBox.getValue().equals("InventoryManager")){
+            SceneSwitcher.switchScene("InventoryManager-view.fxml", actionEvent);
+        }
     }
 }
