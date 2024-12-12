6e7ff72904dc63d4aa3461bd40eddafc0b84b35a
sunjoy6767 <sonjoyp6767@gmail.com>
2024-12-12 12:16:17 +0600

    ..

--- src/main/java/reconditionedcarimporter/group71_2330031_2330190/CustomsClearanceViewController.java
+++ src/main/java/reconditionedcarimporter/group71_2330031_2330190/CustomsClearanceViewController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -35,7 +35,7 @@ public class CustomsClearanceViewController
     @javafx.fxml.FXML
     private ToggleGroup dutyPaidStatusRadioButton;
 
-    private ArrayList<CustomsClearance> customsDutyList;
+    //private ArrayList<CustomsClearance> customsDutyList;
     @FXML
     private RadioButton dutyStatusUnpaidRadioButton;
     @FXML
@@ -51,7 +51,7 @@ public class CustomsClearanceViewController
 
     @FXML
     public void initialize() {
-        customsDutyList = new ArrayList<CustomsClearance>();
+       // customsDutyList = new ArrayList<CustomsClearance>();
 
         clearanceIdCol.setCellValueFactory(new PropertyValueFactory<CustomsClearance, String>("clearanceId"));
         customsAgentNameCol.setCellValueFactory(new PropertyValueFactory<CustomsClearance, String>("customsAgentName"));
@@ -63,8 +63,31 @@ public class CustomsClearanceViewController
 
     @javafx.fxml.FXML
     public void ShowDetailsInTheTableButtonOnAction(ActionEvent actionEvent) {
-        for (CustomsClearance customsClearance : customsDutyList) {
-            customsClearanceTableView.getItems().add(customsClearance);
+        FileInputStream fis=null;
+        ObjectInputStream ois=null;
+
+        try{
+            File f = new File("CustomsClearance.bin");
+            if(f.exists()){
+                fis = new FileInputStream(f);
+            }
+            else{
+                //Alert: file does not exist
+            }
+            if(fis != null) ois = new ObjectInputStream(fis);
+
+            while(true) {
+                customsClearanceTableView.getItems().add(
+                        (CustomsClearance) ois.readObject());
+            }
+        }
+        catch(Exception e){
+            try {
+                if (ois != null) ois.close();
+            }
+            catch(Exception e2){
+                //
+            }
         }
     }
 
@@ -86,15 +109,28 @@ public class CustomsClearanceViewController
             clearanceStatus = "Pending";
         }
 
-        CustomsClearance cc = new CustomsClearance(
-                clearanceIdTextField.getText(),
-                customsAgentNameTextField.getText(),
-                clearanceStatus,
-                paidStatus,
-                customsDutyAmountTextField.getText(),
-                clearanceDatePicker.getValue()
-        );
-        customsDutyList.add(cc);
+        try{
+            File f = new File("CustomsClearance.bin");
+            FileOutputStream fos = null;
+            ObjectOutputStream oos = null;
+            if(f.exists()){
+                fos = new FileOutputStream(f,true);
+                oos = new AppendableObjectOutputStream(fos);;
+            }
+            else {
+                fos = new FileOutputStream(f);
+                oos = new ObjectOutputStream(fos);
+            }
+
+            oos.writeObject(new CustomsClearance(clearanceIdTextField.getText(), customsAgentNameTextField.getText(),
+                    Double.parseDouble(customsDutyAmountTextField.getText()), clearanceStatus,
+                    paidStatus, clearanceDatePicker.getValue()));
+
+            oos.close();
+        }
+        catch(Exception e){
+            //
+        }
     }
 
     @javafx.fxml.FXML
