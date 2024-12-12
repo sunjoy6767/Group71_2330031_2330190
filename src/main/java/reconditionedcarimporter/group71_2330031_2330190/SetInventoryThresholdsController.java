e84c74e2e9245dea99af3d6eee32a30b5b138466
zawad12 <2330031@iub.edu.bd>
2024-12-12 22:01:17 +0600

    ...

--- src/main/java/reconditionedcarimporter/group71_2330031_2330190/SetInventoryThresholdsController.java
+++ src/main/java/reconditionedcarimporter/group71_2330031_2330190/SetInventoryThresholdsController.java
@@ -3,5 +3,27 @@ package reconditionedcarimporter.group71_2330031_2330190;
 public class SetInventoryThresholdsController
 {
     @javafx.fxml.FXML
+    private TableColumn minimumThresholdCol;
+    @javafx.fxml.FXML
+    private TableColumn typeOfProductCol;
+    @javafx.fxml.FXML
+    private TextField minimumThresholdTextField;
+    @javafx.fxml.FXML
+    private TableView tableViewThreshold;
+    @javafx.fxml.FXML
+    private TextField maximumThresholdTextField;
+    @javafx.fxml.FXML
+    private TableColumn maximumThresholdCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void goBackToInvenoryManagerButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void setInventoryThresholdButtonOnAction(ActionEvent actionEvent) {
+    }
+}
